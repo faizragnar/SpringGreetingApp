7267045b43838d44e6edf080ed65031cb00db7e4
unknown <faiyazamail@gmail.com>
2025-03-03 16:23:39 +0530

    [Faiyaj ahmed] Add . List all elements

--- src/main/java/org/example/greetingspring/GreetingService.java
+++ src/main/java/org/example/greetingspring/GreetingService.java
@@ -14,6 +14,24 @@ public class GreetingService {
     public GreetingService(GreetingRepository greetingRepository) {
         this.greetingRepository = greetingRepository;
     }
+    @GetMapping("/{name}")
+    public Greeting getGreet(@PathVariable String name){
+        return new Greeting("Hello, "+name+"!");
+    }
+    @PostMapping("/post")
+    public Greeting postGreeting(@RequestBody Greeting greeting) {
+        return new Greeting("Received: " + greeting.getMessage());
+    }
+
+    @PutMapping("/update/{name}")
+    public Greeting putGreeting(@PathVariable String name, @RequestBody Greeting greeting) {
+        return new Greeting("Updated: " + name + " -> " + greeting.getMessage());
+    }
+
+    @DeleteMapping("/remove/{name}")
+    public Greeting deleteGreeting(@PathVariable String name) {
+        return new Greeting("Deleted: " + name);
+    }
 
     public Greeting saveGreeting(Greeting greeting) {
         return greetingRepository.save(greeting);
