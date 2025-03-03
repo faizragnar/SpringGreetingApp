4d5685b2554daf6e0dd20ffb82345cf583fe38a6
unknown <faiyazamail@gmail.com>
2025-03-03 15:31:50 +0530

    [Faiyaj ahmed] Add . Save message in repo

--- src/main/java/org/example/greetingspring/GreetingControler.java
+++ src/main/java/org/example/greetingspring/GreetingControler.java
@@ -19,24 +19,5 @@ public class GreetingControler{
         return greetingService.saveGreeting(greeting);
     }
 
-    @GetMapping("/{id}")
-    public Greeting getGreetingById(@PathVariable Long id) {
-        return greetingService.getGreetingById(id);
-    }
 
-    @GetMapping("/all")
-    public List<Greeting> getAllGreetings() {
-        return greetingService.getAllGreetings();
-    }
-
-    @PutMapping("/update/{id}")
-    public Greeting updateGreeting(@PathVariable Long id, @RequestBody Greeting greeting) {
-        return greetingService.updateGreeting(id, greeting);
-    }
-
-    @DeleteMapping("/delete/{id}")
-    public String deleteGreeting(@PathVariable Long id) {
-        greetingService.deleteGreeting(id);
-        return "Greeting deleted successfully!";
-    }
 }
--- src/main/java/org/example/greetingspring/GreetingService.java
+++ src/main/java/org/example/greetingspring/GreetingService.java
@@ -20,23 +20,4 @@ public class GreetingService {
         return greetingRepository.save(greeting);
     }
 
-    public Greeting getGreetingById(Long id) {
-        Optional<Greeting> greeting = greetingRepository.findById(id);
-        return greeting.orElse(new Greeting("Greeting not found!"));
-    }
-
-    public List<Greeting> getAllGreetings() {
-        return greetingRepository.findAll();
-    }
-
-    public Greeting updateGreeting(Long id, Greeting updatedGreeting) {
-        return greetingRepository.findById(id).map(g -> {
-            g.setMessage(updatedGreeting.getMessage());
-            return greetingRepository.save(g);
-        }).orElse(new Greeting("Greeting not found!"));
-    }
-
-    public void deleteGreeting(Long id) {
-        greetingRepository.deleteById(id);
-    }
 }
